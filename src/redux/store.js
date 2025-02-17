7399a382ce803cb6e356c772c1db3522df747bc8
Swaszz <swathykrishna2227@gmail.com>
2025-02-17 12:17:22 +0530

    modify

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/userSlice";
 import cartReducer from "../redux/features/CartSlice"
 import deliveryReducer from "./features/DeliverySlice";
-import orderReducer from "../redux/features/orderSlice";
+import orderReducer from "./features/orderSlice"; 
 import ownerReducer from "../redux/features/OwnerSlice";
 import adminReducer from "../redux/features/AdminSlice";
 import paymentReducer from "../redux/features/PaymentSlice"
