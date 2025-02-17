90ab9bcda20d3e75b8d7ea12f6e0745708538369
Swaszz <swathykrishna2227@gmail.com>
2025-02-17 13:28:49 +0530

    Fixed DarkMode import path

--- src/components/user/Header.jsx
+++ src/components/user/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import DarkMode from "../../shared/Darkmode";
+import DarkMode from "../shared/DarkMode";
 
 const Header = () => {
   const navigate = useNavigate();
