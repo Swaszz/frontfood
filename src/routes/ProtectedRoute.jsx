f895071560a71d99c94bf62cf20639417118aa67
Swaszz <swathykrishna2227@gmail.com>
2025-03-04 16:30:04 +0530

    made changes

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -6,33 +6,33 @@ function ProtectedRoute({ role, children }) {
   const navigate = useNavigate();
 
   const isUserAuth = useSelector((state) => state.user.isUserAuth);
+
   const isOwnerAuth = useSelector((state) => state.owner.isOwnerAuth);
   const isAdminAuth = useSelector((state) => state.admin.isAdminAuth);
 
   const [isChecked, setIsChecked] = useState(false);
 
   useEffect(() => {
-    const storedUserData = JSON.parse(localStorage.getItem("userData"));
-
-    // Immediately redirect if no user is found
-    if (!storedUserData) {
-      console.warn("User is not authenticated - Redirecting to /login");
-      window.location.href = "/login"; // Ensure full page reload
+    if (isUserAuth === null || isOwnerAuth === null || isAdminAuth === null) {
       return;
     }
-
     const isAuthenticated =
       (role === "user" && isUserAuth) ||
       (role === "restaurantowner" && isOwnerAuth) ||
       (role === "admin" && isAdminAuth);
 
+    console.log(`Checking authentication for role: ${role}`);
+    console.log(
+      `User Auth: ${isUserAuth}, Owner Auth: ${isOwnerAuth}, Admin Auth: ${isAdminAuth}`
+    );
     if (!isAuthenticated) {
       console.warn("Unauthorized - Redirecting to /login");
       window.location.href = "/login";
+      //navigate("/login");
     } else {
       setIsChecked(true);
     }
-  }, [isUserAuth, isOwnerAuth, isAdminAuth, role, navigate]);
+  }, [isUserAuth, isOwnerAuth, isAdminAuth, navigate]);
 
   if (!isChecked) {
     return <div>Loading...</div>;
