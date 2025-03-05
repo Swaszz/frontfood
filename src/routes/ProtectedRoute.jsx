6b4777cd79235265b58a6d65aed0e491d7c96f73
Swaszz <swathykrishna2227@gmail.com>
2025-03-05 21:55:04 +0530

    protected route

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,58 +1,32 @@
 import { useEffect } from "react";
-import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function ProtectedRoute({ role, children }) {
   const navigate = useNavigate();
-  const location = useLocation();
-
-  const userToken = localStorage.getItem("userToken");
-  const ownerToken = localStorage.getItem("ownerToken");
-  const adminToken = localStorage.getItem("adminToken");
 
   const isUserAuth = useSelector((state) => state.user.isUserAuth);
   const isOwnerAuth = useSelector((state) => state.owner.isOwnerAuth);
-  const isAdminAuth = useSelector((state) => state.admin?.isAdminAuth);
+  const isAdminAuth = useSelector((state) => state.admin.isAdminAuth);
 
   useEffect(() => {
-    let isAuthenticated = false;
-    let redirectPath = "/";
+    const storedUserData = localStorage.getItem("userData");
+    const storedOwnerData = localStorage.getItem("ownerData");
 
-    if (role === "user" && isUserAuth && userToken) {
-      isAuthenticated = true;
-      redirectPath = "/user/profile";
-    } else if (role === "restaurantowner" && isOwnerAuth && ownerToken) {
-      isAuthenticated = true;
-      redirectPath = "/owner/profile";
-    } else if (role === "admin" && isAdminAuth && adminToken) {
-      isAuthenticated = true;
-      redirectPath = "/admin/dashboard";
-    }
+    const isAuthenticated =
+      (role === "user" && isUserAuth && storedUserData) ||
+      (role === "restaurantowner" && isOwnerAuth && storedOwnerData) ||
+      (role === "admin" && isAdminAuth);
 
     console.log(`Checking authentication for role: ${role}`);
-    console.log(`User Token: ${userToken ? "Exists" : "Not Found"}`);
-    console.log(`Owner Token: ${ownerToken ? "Exists" : "Not Found"}`);
-    console.log(`Admin Token: ${adminToken ? "Exists" : "Not Found"}`);
     console.log(
       `User Auth: ${isUserAuth}, Owner Auth: ${isOwnerAuth}, Admin Auth: ${isAdminAuth}`
     );
 
     if (!isAuthenticated) {
       navigate("/", { replace: true });
-    } else if (location.pathname !== redirectPath) {
-      navigate(redirectPath, { replace: true });
     }
-  }, [
-    isUserAuth,
-    isOwnerAuth,
-    isAdminAuth,
-    role,
-    navigate,
-    userToken,
-    ownerToken,
-    adminToken,
-    location.pathname,
-  ]);
+  }, [isUserAuth, isOwnerAuth, isAdminAuth, role, navigate]);
 
   return children ? children : <Outlet />;
 }
